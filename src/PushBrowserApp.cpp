7d805e832139d5331fdc8259b6483b1c0cfb853e
toddberreth <todd@studioberreth.io>
2015-06-12 13:57:05 -0400

    initial commit

---
+++ src/PushBrowserApp.cpp
@@ -0,0 +1,71 @@
+#include "pushBrowserApp.h"
+
+//--------------------------------------------------------------
+void pushBrowserApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void pushBrowserApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
